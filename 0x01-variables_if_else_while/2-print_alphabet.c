4e3c07977fb27c272ccd549e4d056470ec1e39bf
Marwanzoi <alopapa556@gmail.com>
2023-08-13 17:53:18 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,13 +11,10 @@
 int main(void)
 {
     char ch;
-
     for (ch = 'a'; ch <= 'z'; ch++)
     {
         putchar(ch);
     }
-
-    putchar('\n'); /* Print a newline character */
-
+    putchar('\n');
     return 0;
 }

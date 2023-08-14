a187278e2672d5722392667694c4b203774db134
Marwanzoi <alopapa556@gmail.com>
2023-08-14 06:12:14 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,11 +10,11 @@
  */
 int main(void)
 {
-    int n;
+int n;
 
-    for (n = 0; n < 10; n++)
-    printf("%i", n);
-    putchar('\n');
+for (n = 0; n < 10; n++)
+printf("%i", n);
+putchar('\n');
 
-    return (0);
+return (0);
 }

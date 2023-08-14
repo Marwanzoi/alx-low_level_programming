785a4cbf6f4e6edbcb629d77f0bbe3a2a67bb343
Marwanzoi <alopapa556@gmail.com>
2023-08-14 06:48:04 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{   
+char c = 'z';
+    
+while (c >= 'a')
+{
+
+putchar(c);
+c--;
+
+}
+putchar("\n");
+return (0);
+}
+

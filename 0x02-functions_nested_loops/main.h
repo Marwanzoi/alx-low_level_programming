22f2713bf8a150fdf97f7456d8a1ffe3f085f118
Marwanzoi <alopapa556@gmail.com>
2023-08-16 05:21:35 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,21 +2,11 @@
 #define MAIN
 /*
  *Main - The Main function
- *Description : Here where you'll find every function's declaration
+ *Description : Here where you will find every function's declaration
 */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void time_teble(void);
-int add(int, int);
-void print_to_98(int n);
-void print_time_table(int n);
 
 
 

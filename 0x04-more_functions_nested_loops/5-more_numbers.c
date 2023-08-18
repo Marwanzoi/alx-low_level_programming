42a1f3eb80149adc5febe0ee4eb4dfcd4bf53650
Marwanzoi <alopapa556@gmail.com>
2023-08-18 08:20:51 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,21 @@
  */
 void more_numbers(void)
 {
-int i, j;
-for (i = 0; i <= 10; i++)
-{
-for (j = 0; j <= 14; j++)
-{
-if (j >= 10)
-{
-_putchar(j / 10 + '0');
-}
-_putchar(j % 10 + '0');
-}
-_putchar('\n');
-}
+	int i;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+				_putchar(i % 10 + '0');
+			i++;
+		}
+	_putchar('\n');
+	}
 }

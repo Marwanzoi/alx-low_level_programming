76ea3436889d313bee7cc9f2f18192c47a223bc9
Marwanzoi <alopapa556@gmail.com>
2023-08-22 12:01:27 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,14 +7,14 @@
  * @s: the string to calculate it's length.
  * Return: length of a string.
  */
-
 int _strlen(char *s)
 {
-int length;
-while (*s++ != '\0')
-{
-s++;
-length++;
-}
-return (length);
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
 }
